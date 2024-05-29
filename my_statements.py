bc3572f2e67adda94de26afe09a9eeac37ba779c
niikun <123465566+niikun@users.noreply.github.com>
2024-05-29 14:13:55 +0000

    initial commit

---
+++ my_statements.py
@@ -0,0 +1,8 @@
+my_list = ["apple","cherry","lemon"]
+my_dict = {"dirnk":"coffee","drink":"tea"}
+
+for list_item in my_list:
+    print(list_item)
+
+for key,val in my_dict.items():
+    print(key,val)
