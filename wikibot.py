1666c28cce69ebd254043a5d7cb210465bb15bf3
niikun <123465566+niikun@users.noreply.github.com>
2024-05-29 15:46:15 +0000

    wikibot update

--- wikibot.py
+++ wikibot.py
@@ -1,26 +1,14 @@
 import wikipedia
-# import ipython
 import click
 
-
-def scrape(word="Micorsoft",length=1):
-    result = wikipedia.summary(word,sentences=length)
+def scrape(word="Microsoft", length=1):
+    result = wikipedia.summary(word, sentences=length)
     return result
 
 @click.command()
-@click.argument('word')
+@click.argument("word")
 def main(word):
     print(scrape(word))
 
 if __name__ == "__main__":
-    main()
-
-# import requests
-# from bs4 import BeautifulSoup
-
-# TARGET_URL = "https://en.wikipedia.org/wiki/OpenAI"
-
-# res = requests.get(TARGET_URL)
-# soup = BeautifulSoup(res.text,"html.parser")
-
-# print(soup.text)
+    main()  # pylint: disable=no-value-for-parameter
