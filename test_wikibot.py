7dec17c74244bde6b1f2c36359ff343a26562763
niikun <123465566+niikun@users.noreply.github.com>
2024-05-31 13:06:14 +0000

    test wikibot

--- test_wikibot.py
+++ test_wikibot.py
@@ -8,7 +8,7 @@ def test_scrape():
 
 
 def test_wikibot():
-  runner = CliRunner()
-  result = runner.invoke(cli, ['--word','OpenAI'])
-  assert result.exit_code == 0
-  assert result.output == 'OpenAI is an American artificial intelligence (AI) research organization founded in December 2015, researching artificial intelligence with the goal of developing "safe and beneficial" artificial general intelligence, which it defines as "highly autonomous systems that outperform humans at most economically valuable work." As one of the leading organizations of the AI boom, it has developed several large language models, advanced image generation models, and previously, released open-source models.\n'
+    runner = CliRunner()
+    result = runner.invoke(cli, ['--word','OpenAI'])
+    assert result.exit_code == 0
+    assert result.output == 'OpenAI is an American artificial intelligence (AI) research organization founded in December 2015, researching artificial intelligence with the goal of developing "safe and beneficial" artificial general intelligence, which it defines as "highly autonomous systems that outperform humans at most economically valuable work." As one of the leading organizations of the AI boom, it has developed several large language models, advanced image generation models, and previously, released open-source models.\n'
--- wikibot.py
+++ wikibot.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-
-import wikipedia
 import click
 from mylib.bot import scrape
 
