d4c795d52bb18000c02bdaaec73838af8ab53545
niikun <123465566+niikun@users.noreply.github.com>
2024-05-31 14:33:12 +0000

    fastapi

---
+++ app.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+import uvicorn
+from mylib.bot import scrape
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Wiki(BaseModel):
+    word:str
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/word/{word}")
+def scrape_word(word: str):
+    scraped = scrape(word)  # word is now an instance of Wiki
+    return {"word": word, "ans": scraped}
+
+@app.post("/wiki")
+async def scrape_story(wiki: Wiki):
+    result = scrape(word=wiki.word)
+    payload = {"wikipage": result}
+    json_compatible_item_data = jsonable_encoder(payload)
+    return JSONResponse(content=json_compatible_item_data)
